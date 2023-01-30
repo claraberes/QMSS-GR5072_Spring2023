62bc87a039d129a3998bade0245ff7eb2bfff11f
claraberes <clara.beres@gmail.com>
2023-01-29 22:17:06 -0500

    baseline

---
+++ Week 2/4 Final Reports or Deliverables/HW 1 - Practicing Commits.py
@@ -0,0 +1,19 @@
+print('hello world')
+print('hello professor')
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+substraction = num1 - num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Display the subtraction
+print('The substraction of {0} and {1} is {2}'.format(num1, num2, substraction))
+
+
